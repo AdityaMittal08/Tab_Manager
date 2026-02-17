f50de1bfe0a1488dd70e438a95bbb6b5c95d65a1
AdityaMittal08 <mittaladitya0801@gmail.com>
2026-02-17 22:21:52 +0530

    changes in text

--- tab-manager/src/components/Title.jsx
+++ tab-manager/src/components/Title.jsx
@@ -19,12 +19,12 @@ const Features = [
     statement: "Built with React for a snappy, responsive experience.",
   },
   {
-    topic: "Pin tabs ",
+    topic: "Pin tabs: ",
     statement: "Quickly access your important tabs.",
   },
   {
-    topic: "Drag and drop",
-    statement: "Change your tabs from one window to another rapidly.",
+    topic: "Drag and drop: ",
+    statement: "Move your tabs from one window to another rapidly.",
   },
 ];
 
